a54a46b165745f4084e70e5f8e3c22b828bdbea2
Ana Walton <awalton3@nd.edu>
2019-06-19 17:04:05 -0400

    committing

---
+++ mugsite/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "secure",
+    authDomain: "mugsite-95f7c.firebaseapp.com",
+    databaseURL: "https://mugsite-95f7c.firebaseio.com",
+    projectId: "mugsite-95f7c",
+    storageBucket: "mugsite-95f7c.appspot.com",
+    messagingSenderId: "481328336718",
+    appId: "1:481328336718:web:41e04f8894a2cdd1"
+  }
+};
