f283b608bba5e43ec627bbb2deb6f9ca614de6b3
Ana Walton <awalton3@nd.edu>
2019-06-21 10:56:32 -0400

    refactored, cleaned up user and auth service to better carry out auth and user related task in the app

--- mugsite/src/app/mughub/auth/auth.service.ts
+++ mugsite/src/app/mughub/auth/auth.service.ts
@@ -1,41 +1,17 @@
 import * as firebase from 'firebase';
 import { Injectable } from '@angular/core';
 import { UserService } from 'src/app/shared/services/user.service';
-import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({ providedIn: 'root' })
 
 export class AuthService {
 
-  constructor(
-    private userService: UserService,
-    private db: AngularFirestore
-  ) {}
+  constructor(private userService: UserService) {}
 
   register(formData: { email: string; password: string; name: any; type: string; }) {
     firebase.auth().createUserWithEmailAndPassword(formData.email, formData.password)
       .then(userObj => {
-
-        //add user to Firebase User collection
-        this.db.collection('/users')
-          .doc(userObj.user.uid)
-          .set({
-            name: formData.name,
-            photoUrl: '',
-            email: userObj.user.email,
-            type: formData.type,
-            uid: userObj.user.uid
-          });
-
-        //update local userObj
-        this.userService.updateLocalUser(
-          userObj.user.displayName,
-          userObj.user.photoURL,
-          userObj.user.email,
-          formData.type,
-          userObj.user.uid
-        );
-
+        this.userService.addUserToFbCollect(formData.name, null, userObj.user.email, formData.type, userObj.user.uid);
       })
       .catch(error => { alert(error.message) })
   }
@@ -43,16 +19,7 @@ export class AuthService {
   login(formData: { email: string; password: string; }) {
     firebase.auth().signInWithEmailAndPassword(formData.email, formData.password)
       .then(userObj => {
-        this.db.collection('/users').doc(userObj.user.uid).get()
-          .subscribe(userObj => {
-            this.userService.updateLocalUser(
-              userObj.data().name,
-              userObj.data().photoUrl,
-              userObj.data().email,
-              userObj.data().type,
-              userObj.data().uid
-            )
-          })
+        this.userService.createLocalUser(userObj.user.uid)
       })
       .catch(error => { alert(error.message) })
   }
--- mugsite/src/app/shared/services/user.service.ts
+++ mugsite/src/app/shared/services/user.service.ts
@@ -1,36 +1,60 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from '../models/user.model';
-import { Subject } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 
 export class UserService {
 
-  private user: User = {
-    name: '',
-    photoUrl: '',
-    email: '',
-    type: '',
-    uid: ''
-  }
+  private currentUser: User;
 
-  userSub = new Subject<User>();
+  user = new Subject<User>();
 
-  constructor() { }
-
-  updateLocalUser(name: string, photoUrl: string, email: string, type: string, uid: string) {
-    //parameters are null if they should not be updated
-    if (name) this.user.name = name;
-    if (photoUrl) this.user.photoUrl = photoUrl;
-    if (email) this.user.email = email;
-    if (type) this.user.type = type;
-    if (uid) this.user.uid = uid;
-    this.userSub.next(this.user);
-  }
+  constructor(private db: AngularFirestore) { }
 
   getLocalUser() {
-    return this.user;
+    return this.currentUser;
   }
+
+  getUserFromFbCollect(uid: string) {
+    return this.db.collection('/users').doc(uid).get();
+  }
+
+  addUserToFbCollect(name: string, photoUrl: string, email: string, type: string, uid: string) {
+    this.db.collection('/users')
+      .doc(uid)
+      .set({
+        name: name,
+        photoUrl: photoUrl,
+        email: email,
+        type: type,
+        uid: uid
+      });
+  }
+
+  createLocalUser(uid: string) {
+    let userSub: Subscription = this.getUserFromFbCollect(uid)
+      .subscribe(userObj => {
+        this.currentUser = new User(
+          userObj.data().name,
+          userObj.data().photoUrl,
+          userObj.data().email,
+          userObj.data().type,
+          userObj.data().uid,
+          true)
+      })
+    userSub.unsubscribe();
+  }
+
+  // updateLocalUser(name: string, photoUrl: string, email: string, type: string, uid: string) {
+  //   //parameters are null if they should not be updated
+  //   if (name) this.currentUser.name = name;
+  //   if (photoUrl) this.currentUser.photoUrl = photoUrl;
+  //   if (email) this.currentUser.email = email;
+  //   if (type) this.currentUser.type = type;
+  //   if (uid) this.currentUser.uid = uid;
+  //   this.user.next(this.currentUser);
+  // }
 
 }
