1372a6ed9af308ec4bd602866442e2025051f60b
Ana Walton <awalton3@nd.edu>
2019-07-02 19:38:41 -0400

    fixed slow image loading problems

--- mugsite/src/app/mughub/welcome/welcome-setup/welcome-setup-profile/profile-bgimage/profile-bgimage.component.ts
+++ mugsite/src/app/mughub/welcome/welcome-setup/welcome-setup-profile/profile-bgimage/profile-bgimage.component.ts
@@ -15,15 +15,16 @@ export class ProfileBgimageComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.urls = [
-      'https://i.ibb.co/6mfPz9G/7.jpg',
-      'https://i.ibb.co/T0fbXbH/6.jpg',
-      'https://i.ibb.co/9VYq6wc/5.jpg',
-      'https://i.ibb.co/HDBt1m4/4.jpg',
-      'https://i.ibb.co/0C9znsS/3.jpg',
-      'https://i.ibb.co/tx4fJ4y/2.jpg',
-      // 'https://i.ibb.co/ZX47Jg6/1.jpg'
+      'https://i.ibb.co/pjG5Rkf/4k-wallpaper-astronomy-evening-2085998.jpg',
+      'https://i.ibb.co/PQyS52p/adventure-automobile-classic-2533092.jpg',
+      'https://i.ibb.co/hKqTZzd/alley-architecture-buildings-2119106.jpg',
+      'https://i.ibb.co/kDGtKr8/anniversary-artificial-beautiful-1803911.jpg',
+      'https://i.ibb.co/L9Bwcn0/bloom-blossom-depth-of-field-2184346.jpg',
+      'https://i.ibb.co/hfhy5z1/branded-converse-all-star-converse-all-star-2421374.jpg',
+      'https://i.ibb.co/0VZMXZd/cobblestone-granite-pebbles-1029604.jpg',
+      'https://i.ibb.co/tQFP2K5/environment-flora-foliage-2537632.jpg',
     ]
-    this.imageClicked = 'https://i.ibb.co/ZX47Jg6/1.jpg';
+    this.imageClicked = 'https://i.ibb.co/pjG5Rkf/4k-wallpaper-astronomy-evening-2085998.jpg';
   }
 
   ngAfterViewInit() {
