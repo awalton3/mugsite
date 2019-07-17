61d2ab58cfcb87aa8f9166d894300b67c4043182
Ana Walton <awalton3@nd.edu>
2019-07-17 12:00:12 -0400

    added cases of overflow to calendar so that it adjusts years automatically. Calendar logic seems to be sound

--- mugsite/src/app/mughub/tutor-app/hour-log/hour-log.component.ts
+++ mugsite/src/app/mughub/tutor-app/hour-log/hour-log.component.ts
@@ -10,14 +10,14 @@ export class HourLogComponent implements OnInit {
 
   constructor(private sidenavService: SidenavService) { }
   displayedMonth: { num: number; name: string };
+  displayedYear: number;
   days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   ngOnInit() {
-    this.displayedMonth = {
-      num: new Date().getMonth() + 1,
-      name: this.months[new Date().getMonth()]
-    }
+    let currMonth = new Date().getMonth() + 1
+    this.displayedMonth = { num: currMonth, name: this.months[currMonth - 1] };
+    this.displayedYear = new Date().getFullYear();
   }
 
   closeSidenav() {
@@ -33,18 +33,36 @@ export class HourLogComponent implements OnInit {
     return range;
   }
 
-  getStartDayInMonth(month, year) {
+  getStartDayInMonth(month: number, year: number) {
     return new Date(year, month - 1, 0).getDay() + 1;
   }
 
   onNextMonth() {
-    this.displayedMonth.num++;
-    this.displayedMonth.name = this.months[this.displayedMonth.num - 1];
+    if (this.checkOverflow('next')) {
+      this.displayedYear++;
+      this.displayedMonth = { num: 1, name: this.months[0] };
+    } else {
+      this.displayedMonth.num++;
+      this.displayedMonth.name = this.months[this.displayedMonth.num - 1];
+    }
   }
 
   onPrevMonth() {
-    this.displayedMonth.num--; 
-    this.displayedMonth.name = this.months[this.displayedMonth.num - 1];
+    if (this.checkOverflow('prev')) {
+      this.displayedYear--;
+      this.displayedMonth = { num: 12, name: this.months[11] };
+    } else {
+      this.displayedMonth.num--;
+      this.displayedMonth.name = this.months[this.displayedMonth.num - 1];
+    }
+  }
+
+  checkOverflow(action: string) {
+    let currMonth = this.displayedMonth.num;
+    if (action === 'next')
+      return currMonth + 1 > 12
+    else
+      return currMonth - 1 < 1;
   }
 
 }
