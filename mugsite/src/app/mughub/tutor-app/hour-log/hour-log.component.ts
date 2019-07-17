cbdd333766b8e9e2ed5f6b9cd4b8d1da1a71eb9d
Ana Walton <awalton3@nd.edu>
2019-07-17 11:23:37 -0400

    added ability to change month with next and prev buttons

--- mugsite/src/app/mughub/tutor-app/hour-log/hour-log.component.ts
+++ mugsite/src/app/mughub/tutor-app/hour-log/hour-log.component.ts
@@ -9,23 +9,42 @@ import { SidenavService } from '../../sidenav/sidenav.service';
 export class HourLogComponent implements OnInit {
 
   constructor(private sidenavService: SidenavService) { }
+  displayedMonth: { num: number; name: string };
   days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
   ngOnInit() {
-    console.log(this.getNumDaysInMonth(3, 2019));
-    console.log(this.getStartDayInMonth(1, 2019));
+    this.displayedMonth = {
+      num: new Date().getMonth() + 1,
+      name: this.months[new Date().getMonth()]
+    }
   }
 
   closeSidenav() {
     this.sidenavService.onToggle.next();
   }
 
-  getNumDaysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
+  numDaysInMonthRange(month, year) {
+    let rangeMax = new Date(year, month, 0).getDate();
+    let range = [];
+    for (let i = 1; i <= rangeMax; i++) {
+      range.push(i);
+    }
+    return range;
   }
 
   getStartDayInMonth(month, year) {
     return new Date(year, month - 1, 0).getDay() + 1;
   }
 
+  onNextMonth() {
+    this.displayedMonth.num++;
+    this.displayedMonth.name = this.months[this.displayedMonth.num - 1];
+  }
+
+  onPrevMonth() {
+    this.displayedMonth.num--; 
+    this.displayedMonth.name = this.months[this.displayedMonth.num - 1];
+  }
 
 }
