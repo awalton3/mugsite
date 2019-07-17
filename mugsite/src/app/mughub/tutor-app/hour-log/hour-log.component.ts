d4632a68bacc5974f5881195f703a2698318c6c1
Ana Walton <awalton3@nd.edu>
2019-07-17 00:59:20 -0400

    wrote logic to get number of days in each month and the start day for each month using js data methods

--- mugsite/src/app/mughub/tutor-app/hour-log/hour-log.component.ts
+++ mugsite/src/app/mughub/tutor-app/hour-log/hour-log.component.ts
@@ -9,12 +9,23 @@ import { SidenavService } from '../../sidenav/sidenav.service';
 export class HourLogComponent implements OnInit {
 
   constructor(private sidenavService: SidenavService) { }
-
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   ngOnInit() {
+    console.log(this.getNumDaysInMonth(3, 2019));
+    console.log(this.getStartDayInMonth(1, 2019));
   }
 
   closeSidenav() {
     this.sidenavService.onToggle.next();
   }
+
+  getNumDaysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  getStartDayInMonth(month, year) {
+    return new Date(year, month - 1, 0).getDay() + 1;
+  }
+
 
 }
