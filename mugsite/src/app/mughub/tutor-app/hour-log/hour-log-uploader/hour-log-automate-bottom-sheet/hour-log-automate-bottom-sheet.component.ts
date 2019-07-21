a8a12be58a15bca629a1fa322f422f5ade88d0cf
Ana Walton <awalton3@nd.edu>
2019-07-21 00:46:46 -0400

    added day selector and timeframe input to bottom shee

--- mugsite/src/app/mughub/tutor-app/hour-log/hour-log-uploader/hour-log-automate-bottom-sheet/hour-log-automate-bottom-sheet.component.ts
+++ mugsite/src/app/mughub/tutor-app/hour-log/hour-log-uploader/hour-log-automate-bottom-sheet/hour-log-automate-bottom-sheet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hour-log-automate-bottom-sheet',
@@ -8,12 +9,32 @@ import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 })
 export class HourLogAutomateBottomSheetComponent implements OnInit {
 
+  timeframeForm = new FormGroup({});
+  days = ['Sun', 'M', 'Tu', 'W', 'Th', 'F', 'Sat']
+  daysSelected: string[] = [];
+
   constructor(
     private bottomSheetRef: MatBottomSheetRef<HourLogAutomateBottomSheetComponent>,
     // @Inject(MAT_BOTTOM_SHEET_DATA) public data: any,
   ) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.timeframeForm = new FormGroup({
+      timeframe: new FormControl(null, Validators.required)
+    })
+  }
+
+  onDaySelect(day: string) {
+    if (this.daysSelected.includes(day)) {
+      let indexToDelete = this.daysSelected.indexOf(day);
+      this.daysSelected.splice(indexToDelete,1);
+    }
+    else
+      this.daysSelected.push(day);
   }
 
 }
