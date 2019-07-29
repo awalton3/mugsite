3ab28c9eb335d85e6a04c9c4930c8e419fa1ce92
Ana Walton <awalton3@nd.edu>
2019-07-28 21:39:22 -0400

    restyled the login page with new logo and updated ccolor pallet

--- mugsite/src/app/shared/inbox/inbox.component.ts
+++ mugsite/src/app/shared/inbox/inbox.component.ts
@@ -1,24 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SidenavService } from 'src/app/mughub/sidenav/sidenav.service';
 import { Upload } from 'src/app/mughub/tutor-app/uploads/upload.model';
+import { InboxService } from './inbox.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
   styleUrls: ['./inbox.component.css']
 })
-export class InboxComponent implements OnInit {
+export class InboxComponent implements OnInit, OnDestroy {
 
+  private subs = new Subscription();
   testUpload: Upload;
+  uploads: Upload[];
 
-  constructor(private sidenavService: SidenavService) { }
+  constructor(
+    private sidenavService: SidenavService,
+    private inboxService: InboxService
+  ) { }
 
   ngOnInit() {
-    this.testUpload = new Upload(null, null, null, null, null, null, null, null, null); 
+    this.testUpload = new Upload(null, null, null, null, null, null, null, null, null);
+    this.listenForUploads();
+  }
+
+  listenForUploads() {
+    let uploads = [];
+    this.subs.add(this.inboxService.fetchUploads().onSnapshot(querySnapshot => {
+      querySnapshot.forEach(uploadDoc => uploads.push(uploadDoc))
+      this.uploads = uploads;
+      console.log(this.uploads);
+    }))
   }
 
   closeSidenav() {
     this.sidenavService.onToggle.next();
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
 }
---
+++ mugsite/src/app/shared/inbox/inbox.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { UserService } from 'src/app/mughub/auth/user.service';
+
+@Injectable({ providedIn: 'root' })
+
+export class InboxService {
+
+  constructor(private userService: UserService) {}
+
+  fetchUploads() {
+    return firebase.firestore().collection('/uploads')
+      .where('recipient', '==', this.userService.getUserSession())
+      .orderBy('timestamp', 'desc')
+  }
+
+}
